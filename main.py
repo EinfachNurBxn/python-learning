533ff8a9a11fc6a1ef09fed84b874cc2bce80309
EinfachNurBxn <absolutheftigerben@gmail.com>
2026-02-16 21:43:55 +0100

    [MINIGAME "minecraft.py"] add a noclip system and a check how far the block to remove is away from the player

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-v = 23
-
-def get_version():
-    return v
-if __name__ == "__main__":
-    print("Version:", get_version())
+teilnehmer = ["Ben", "Mohammed", "Paul", "Klaus"]
+punkte = [1, 5, 4, 3]
+for i in range(len(teilnehmer)):
+    print(teilnehmer[i], "ist Teilnehmer des Wettbewerbes.", "und hat", punkte[i], "Punkte erreicht.")
--- minigames/minecraft-python/minecraft.py
+++ minigames/minecraft-python/minecraft.py
@@ -11,15 +11,41 @@ for i in range(20):
           texture='grass.png', parent=scene, origin_y=0.5)
     boxes.append(box)
 
+noclip_enabled = False
+
+def update():
+  if noclip_enabled:
+    if held_keys['space']:
+      player.position += Vec3(0, 0.05, 0)
+    if held_keys['shift']:
+      player.position += Vec3(0, -0.05, 0)
+
 def input(key):
-  for box in boxes:
+  global noclip_enabled
+  if key == 'n':
+    noclip_enabled = not noclip_enabled
+    if noclip_enabled:
+      player.gravity = 0
+      print("Noclip aktiviert")
+    else:
+      player.gravity = 1
+      print("Noclip deaktiviert")
+  
+  if noclip_enabled:
+    if key == 'space':
+      player.position += Vec3(0, 0.1, 0)
+    if key == 'shift':
+      player.position += Vec3(0, -0.1, 0)
+  
+  for box in boxes[:]:
     if box.hovered:
       if key == 'left mouse down':
         new = Button(color=color.white, model='cube', position=box.position + mouse.normal,
                     texture='grass.png', parent=scene, origin_y=0.5)
         boxes.append(new)
       if key == 'right mouse down':
-        boxes.remove(box)
-        destroy(box)
-
+        dist = distance(player.position, box.position)
+        if dist <= 5:
+          boxes.remove(box)
+          destroy(box)
 app.run()
