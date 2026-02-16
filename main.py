e89c64b0a9ff3ae46c8e4fada6fc3a3c815a5c86
EinfachNurBxn <absolutheftigerben@gmail.com>
2026-02-16 21:48:47 +0100

    [main.py] add a input for inserting the points than storing it in a seperate, constant list

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 teilnehmer = ["Ben", "Mohammed", "Paul", "Klaus"]
-punkte = [1, 5, 4, 3]
+punkte = input("Geben Sie die Punkte der Teilnehmer ein (getrennt durch Kommas): ").split(",")
+punkte = [int(p.strip()) for p in punkte]  # Konvertiere den Input mit der .strip Funktion in eine Liste von Ganzzahlen
 for i in range(len(teilnehmer)):
     print(teilnehmer[i], "ist Teilnehmer des Wettbewerbes.", "und hat", punkte[i], "Punkte erreicht.")
